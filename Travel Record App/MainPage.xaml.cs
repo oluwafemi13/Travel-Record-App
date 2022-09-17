64a124784430d812b871b65b7d7e82815e389b2f
oluwafemi13 <sundayjabikem@gmail.com>
2022-09-18 00:48:29 +0100

    Add project files.

---
+++ Travel Record App/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Travel_Record_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void register_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
